85a70fb695723113731bf9e5716e7d2383d635a2
46mikail <mikailefecikrikci@gmail.com>
2025-05-08 11:07:55 +0300

    test

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  
+  const fetchData = async () => {
+    const response = await fetch('https://dummyjson.com/carts');
+    const result = await response.json();
+    setData(result.carts); 
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="card-grid">
+          {data.map((item, index) => (
+            <div className="card" key={index}>
+              <h2>Cart ID: {item.id}</h2>
+              <p>Total Products: {item.totalProducts}</p>
+              <p>Total Amount: {item.total}</p>
+            </div>
+          ))}
+        </div>
+      </header>
+    </div>
+  );
+  
+}
+
+export default App;
